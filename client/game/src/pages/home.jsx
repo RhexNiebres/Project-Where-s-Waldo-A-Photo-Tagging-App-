7b00f95882303ac1ede33041686107553f4143df
RhexNiebres <rhexniebres@gmail.com>
2025-04-02 15:11:52 -0800

    setup home page with initial content

---
+++ client/game/src/pages/home.jsx
@@ -0,0 +1,23 @@
+
+const Home = () => {
+  return (
+    <div className=" bg-slate-200 min-h-screen">
+ 
+      <div className="flex items-center justify-center">
+        <h1 className=" rounded text-6xl p-5 font-ui-monospace font-bold">Seek & Spot</h1>
+      </div>
+      <section>
+        <h1>Welcome to Seek & Spot</h1>
+        <p>Hey there , Explorer! Ready to test </p>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </section>
+
+    </div>
+  );
+};
+
+export default Home;
