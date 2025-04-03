56f6f2be4755b8fb7d1c42bebf5001ab86356cb1
RhexNiebres <rhexniebres@gmail.com>
2025-04-02 17:25:21 -0800

    add toggle menu functionality for  toggling menu for smaller screen to access links

--- client/game/src/components/NavBar.jsx
+++ client/game/src/components/NavBar.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBarsStaggered, faRectangleXmark  } from "@fortawesome/free-solid-svg-icons";
+import {
+  faBarsStaggered,
+  faRectangleXmark,
+} from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
-    const [isOpen,setIsopen] = useState(false);
+  const [isOpen, setIsopen] = useState(false);
 
-    const toggleMenu=()=>{
-        setIsopen(!isOpen);
-    }
+  const toggleMenu = () => {
+    setIsopen(!isOpen);
+  };
 
   return (
-    <nav
-      className="fixed top-0 w-full bg-blue-400 shadow-md"
-    >
+    <nav className="fixed top-0 w-full bg-blue-500 shadow-md h-16">
       <div className="flex justify-between items-center px-6 py-4 max-w-7xl mx-auto">
         <Link
           to="/"
@@ -21,39 +22,50 @@ const NavBar = () => {
         >
           Seek & Spot
         </Link>
-            <button onClick={toggleMenu} className="md:hidden text-2xl text-white hover:text-slate-800">
-            <FontAwesomeIcon icon={isOpen ? faRectangleXmark  : faBarsStaggered} />
-            </button>
+        <button
+          onClick={toggleMenu}
+          className="md:hidden text-2xl text-white active:text-slate-800"
+        >
+          <FontAwesomeIcon icon={isOpen ? faRectangleXmark : faBarsStaggered} />
+        </button>
 
         <ul className="hidden md:flex space-x-5 ">
           {["Home", "Leader Board"].map((item) => (
             <li key={item}>
               <Link
-                to={item === "Home" ? "/" : `/${item.toLowerCase().replace(" ", "")}`}
+                to={
+                  item === "Home"
+                    ? "/"
+                    : `/${item.toLowerCase().replace(" ", "")}`
+                }
                 className="text-white text-lg font-semibold hover:text-slate-800"
-                
-              > {item}</Link>
+              >
+                {" "}
+                {item}
+              </Link>
             </li>
           ))}
-          
         </ul>
       </div>
-          {isOpen&&(
-             <ul className="md:hidden flex flex-col items-center bg-blue-500 py-3 space-y-3">
-             {["Home", "Leader Board"].map((item) => (
-               <li key={item}>
-                 <Link
-                   to={item === "Home" ? "/" : `/${item.toLowerCase().replace(" ", "")}`}
-                   className="text-white text-lg font-semibold active:text-slate-800"
-                   onClick={() => setIsOpen(false)} 
-                 >
-                   {item}
-                 </Link>
-               </li>
-             ))}
-           </ul>
-          )}
-      
+      {isOpen && (
+        <ul className="md:hidden flex flex-col items-center bg-blue-500 py-3 space-y-3">
+          {["Home", "Leader Board"].map((item) => (
+            <li key={item}>
+              <Link
+                to={
+                  item === "Home"
+                    ? "/"
+                    : `/${item.toLowerCase().replace(" ", "")}`
+                }
+                className="text-white text-lg font-semibold active:text-slate-800"
+                onClick={() => setIsOpen(false)}
+              >
+                {item}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </nav>
   );
 };
