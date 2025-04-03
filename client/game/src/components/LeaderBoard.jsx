8c4ce7f3810e36a4812dda541b070fcb7fb76282
RhexNiebres <rhexniebres@gmail.com>
2025-04-03 12:39:24 -0800

    set up initial leaderboard content

---
+++ client/game/src/components/LeaderBoard.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const LeaderBoard = () => {
+  return (
+    <>
+      <div>
+      <h1>Leader Board</h1>
+      <ul>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+      </ul>
+      </div>
+    </>
+  );
+};
+
+export default LeaderBoard;
