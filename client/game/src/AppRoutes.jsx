701834eeba0944e00a06bf4403a308dd713bbf70
RhexNiebres <rhexniebres@gmail.com>
2025-04-03 14:17:52 -0800

    feat: import route for Rules page

--- client/game/src/AppRoutes.jsx
+++ client/game/src/AppRoutes.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import App from "./App";
 import LeaderBoard from "./components/LeaderBoard";
-
+import Rules from "./pages/Rules";
 function AppRoutes() {
     return(
         <Routes>
             <Route path="/" element={<App />}/>
             <Route path="/leaderboard" element={<LeaderBoard />}/>
+            <Route path="/Rules" element={<Rules />}/>
         </Routes>
     );
 }
