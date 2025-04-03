51b720b197958c94736679b29e5378eea049f3b6
RhexNiebres <rhexniebres@gmail.com>
2025-04-02 19:25:38 -0800

    style: add styling for github icon

--- client/game/src/components/Footer.jsx
+++ client/game/src/components/Footer.jsx
@@ -4,14 +4,14 @@ import { faSquareGithub } from "@fortawesome/free-brands-svg-icons";
   
 const Footer = () => {
   return (
-    <footer className="fixed bottom-0 left-0 flex flex-row justify-evenly  items-center bg-blue-500 text-white py-3 w-full">
+    <footer className="fixed bottom-0 left-0 flex flex-row justify-evenly  items-center bg-blue-500 text-white py-1 w-full">
       <p>&copy; {new Date().getFullYear()} Rhex R. Niebres</p>
 
-      <a
+      <a 
         href="https://github.com/RhexNiebres"
         target="_blank"
         rel="noopener noreferrer"
-      >
+      className="hover:text-yellow-300 text-4xl">
         <FontAwesomeIcon icon={faSquareGithub} />
       </a>
       <p>All rights reserved.</p>
