9b9ec25bee4108afbdf0e65e607ae19ae2f3d0f5
RhexNiebres <rhexniebres@gmail.com>
2025-04-05 17:57:27 +0800

    import Game page to be displayed by a route

--- client/game/src/AppRoutes.jsx
+++ client/game/src/AppRoutes.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route,} from "react-router-dom";
 import App from "./App";
 import LeaderBoard from "./components/LeaderBoard";
 import Rules from "./pages/Rules";
+import Game from "./pages/Game";
 function AppRoutes() {
-    return(
-        <Routes>
-            <Route path="/" element={<App />}/>
-            <Route path="/leaderboard" element={<LeaderBoard />}/>
-            <Route path="/Rules" element={<Rules />}/>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/leaderboard" element={<LeaderBoard />} />
+      <Route path="/rules" element={<Rules />} />
+      <Route path="/game" element={<Game />} />
+    </Routes>
+  );
 }
 
 export default AppRoutes;
