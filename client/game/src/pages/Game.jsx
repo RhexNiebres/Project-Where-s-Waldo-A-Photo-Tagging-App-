273a0a10a7d906b47408af48c23733a6330987d4
RhexNiebres <rhexniebres@gmail.com>
2025-04-07 16:59:06 +0800

    add initial content for Game page

---
+++ client/game/src/pages/Game.jsx
@@ -0,0 +1,9 @@
+const Game = () =>{
+    return(
+        <div>
+            <h1>this is the game page it needs the map picture, timer and characters dropdown</h1>
+        </div>
+    );
+};
+
+export default Game;
