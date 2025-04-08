df01f409802a6722c28611169de9b62071658922
RhexNiebres <rhexniebres@gmail.com>
2025-04-08 15:36:28 +0800

    chore: import cors that allows the app to communicate to our backend

--- server/src/app.js
+++ server/src/app.js
@@ -3,7 +3,9 @@ const express = require('express');
 const app = express();
 const playerRouter = require('./routes/userRouter');
 const  characterRouter = require("./routes/characterRoutes");
+const cors = require('cors'); 
 
+app.use(cors());
 app.use('/api/player', playerRouter);
 app.use('/api/character', characterRouter)
 app.listen(process.env.APP_PORT, () => {
