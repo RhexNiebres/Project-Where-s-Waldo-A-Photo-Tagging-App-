8c4bb0a0b80e263e65f97ae10a26449239550999
RhexNiebres <rhexniebres@gmail.com>
2025-04-08 16:39:37 +0800

    Refactor: moved route controllers for cleaner and maintainability approach

--- server/src/routes/playerRouter.js
+++ server/src/routes/playerRouter.js
@@ -1,58 +1,8 @@
-import { Router } from "express";
-import { PrismaClient } from "@prisma/client";
+const express = require("express");
+const router = express.Router();
+const playerController = require("../controllers/playerController");
 
-const playerRouter = Router();
-const prisma = new PrismaClient();
+router.get("/", playerController.getPlayers);
+router.post("/", playerController.createPlayer);
 
-playerRouter.get("/players", async (req, res) => {
-  try {
-    const players = await prisma.player.findMany({
-      orderBy: {
-        time: "asc", //sort low to high
-      },
-    });
-
-    if (!players || players.length === 0) {
-      return res
-        .status(404)
-        .json({ success: false, message: "No players found." });
-    }
-
-    res.status(200).json({ success: true, players });
-  } catch (error) {
-    console.error("Error fetching players:", error);
-    res.status(500).json({ success: false, message: "Server error." });
-  }
-});
-
-playerRouter.post("/player", async (req, res) => {
-  try {
-    const { username, time } = req.body;
-
-    if (!username || !time) {
-      return res
-        .status(400)
-        .json({ success: false, message: "Username and time are required." });
-    }
-
-    const newPlayer = await prisma.player.create({
-      data: {
-        username,
-        time,
-      },
-    });
-
-    if (!newPlayer) {
-      return res
-        .status(500)
-        .json({ success: false, message: "Failed to save player." });
-    }
-
-    res.status(201).json({ success: true, player: newPlayer });
-  } catch (error) {
-    console.error("Error saving player:", error);
-    res.status(500).json({ success: false, message: "Server error." });
-  }
-});
-
-export default playerRouter;
+module.exports = router;
