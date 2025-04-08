5e9b47a37440a1915383a1082fc8b08028778bce
RhexNiebres <rhexniebres@gmail.com>
2025-04-08 15:34:30 +0800

    chore: import player and character routes on app

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
+const playerRouter = require('./routes/userRouter');
+const  characterRouter = require("./routes/characterRoutes");
 
-app.all('/', (req, res) => {});
-
+app.use('/api/player', playerRouter);
+app.use('/api/character', characterRouter)
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on port ${process.env.APP_PORT}`);
 });
