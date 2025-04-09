1b90a7d5d7c859ac3ff3d37ed79f4b1e093b4905
RhexNiebres <rhexniebres@gmail.com>
2025-04-09 13:20:05 +0800

    chore: change user Router to playerRouter

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require('express');
 const app = express();
-const playerRouter = require('./routes/userRouter');
+const playerRouter = require('./routes/playerRouter');
 const  characterRouter = require("./routes/characterRoutes");
 const cors = require('cors'); 
 
