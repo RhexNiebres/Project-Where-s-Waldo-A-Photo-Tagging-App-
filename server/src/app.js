dcff9306aa5884810aceebdc68f03e11ce5cf202
RhexNiebres <rhexniebres@gmail.com>
2025-04-10 10:43:09 +0800

    add and import index route for  testing

--- server/src/app.js
+++ server/src/app.js
@@ -3,12 +3,14 @@ const express = require("express");
 const app = express();
 const playerRouter = require("./routes/playersRoutes");
 const characterRouter = require("./routes/charactersRoutes");
+const indexRouter = require("./routes/indexRoutes");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 app.use("/players", playerRouter);
 app.use("/characters", characterRouter);
+app.use("/", indexRouter);
 app.listen(process.env.APP_PORT, () => {
   console.log(`Server is running on port ${process.env.APP_PORT}`);
 });
