77e3211122b20524628887b055158c57748fe6c9
RhexNiebres <rhexniebres@gmail.com>
2025-04-10 18:58:03 +0800

    feat: add play again link that enables players to have better record

--- client/game/src/pages/PlayerForm.jsx
+++ client/game/src/pages/PlayerForm.jsx
@@ -3,7 +3,7 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { submitPlayerData } from '../apiServices/submitPlayer'; 
 import NavBar from '../components/NavBar';
 import Footer from '../components/Footer';
-
+import { Link } from 'react-router-dom';
 const PlayerForm = () => {
   const navigate = useNavigate();
   const location = useLocation();
@@ -19,7 +19,6 @@ const PlayerForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    // Validate that both name and time are set
     if (!playerName) {
       alert('Please enter a username');
       return;
@@ -33,43 +32,47 @@ const PlayerForm = () => {
     console.log("Submitting player data:", { username: playerName, time });
 
     const playerData = {
-      username: playerName, // Ensure to match the API's expected field name
-      time: time, // Ensure time is a valid number
+      username: playerName, 
+      time: time,
     };
 
     try {
       const data = await submitPlayerData(playerData); // Use the API function
 
       console.log('Player created:', data);
-      navigate('/leaderboard'); // Redirect to leaderboard after submission
+      navigate('/leaderboard'); 
     } catch (error) {
-      console.error('Error:', error); // Log error for debugging
-      alert(`Error: ${error.message}`); // Display error message
+      console.error('Error:', error); 
+      alert(`Error: ${error.message}`); 
     }
   };
 
   return (
     <div className="player-form min-h-screen bg-slate-100 flex flex-col">
        <NavBar />
-       <div className="bg-blue-500 flex flex-col items-center justify-items-center max-w-3xl w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] shadow-lg rounded-3xl p-4 text-white text-center font-bold mx-auto mt-24" >
-       <h2 className="text-center text-yellow-300 font-extrabold text-4xl mb-7">Enter your username</h2>
-      <form onSubmit={handleSubmit} className="flex flex-col items-center bg-slate-100 p-4 rounded-2xl text-blue-500">
+       <div className="bg-blue-500 flex flex-col items-center justify-items-center max-w-3xl w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] shadow-lg rounded-3xl p-7 text-white text-center font-bold mx-auto mt-50 mb-5" >
+       <h2 className="text-center text-yellow-300 font-extrabold text-4xl m-3">You Did It, Champion!🏆</h2>
+       <p className='max-w-xs mx-auto pb-4'>Now, its time to enter your name! and join the ranks of the greats! </p>
+      <form onSubmit={handleSubmit} className="flex flex-col items-center bg-slate-100 p-4 rounded-2xl text-blue-500 shadow-2xl">
         <input
           type="text"
           value={playerName}
           onChange={(e) => setPlayerName(e.target.value)}
           placeholder="Username"
-          className="mb-4 px-4 py-2 border border-slate-300 rounded text-center"
+          className="mb-4 px-4 py-3 border shadow-md border-slate-300 rounded text-center"
         />
-        <button type="submit" className="bg-white text-2xl text-blue-600  px-5 p-1 border rounded-2xl hover:bg-yellow-300 transition-transform duration-300 ease-in-out hover:scale-110 active:scale-95 shadow-md hover:shadow-xl">
+        <h3 className='p-3'>Your Time is: {time.toFixed(1)} seconds</h3>
+        <button type="submit" className="bg-blue-500 text-2xl text-yellow-300  px-5 p-1 rounded-2xl transition-transform duration-300 ease-in-out hover:scale-110 active:scale-95 shadow-md hover:shadow-xl">
           Submit
         </button>
       </form>
-      <div className="text-center mt-4">
-        <h3>Time: {time.toFixed(1)} seconds</h3>
-      </div>
        </div>
-      
+        
+      <div className='bg-blue-500 flex flex-col items-center justify-items-center max-w-3xl w-[90%] sm:w-[80%] md:w-[70%] lg:w-[60%] xl:w-[50%] shadow-lg rounded-3xl p-4 text-white text-center font-bold mx-auto mt-10'>
+      <h1 className='text-yellow-300 text-4xl p-5 font-extrabold leading-snug'  >was that your best?🤔</h1>
+      <p className='max-w-xs mx-auto pb-4 text-xl'>Warning: Your current record will be lost if you press the button.😓</p>
+    <Link to="/rules" className='bg-slate-100 text-2xl text-blue-500 font-extrabold p-2 rounded-2xl transition-transform duration-300 ease-in-out hover:scale-110 active:scale-95 shadow-md hover:shadow-xl hover:bg-yellow-300 hover:blur-none '>Play Again?</Link>
+      </div>
       <Footer/>
     </div>
   );
