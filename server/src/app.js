fc6d0fbaa7582c87e5973e166f8473163b5c2a2b
RhexNiebres <rhexniebres@gmail.com>
2025-04-10 14:10:21 +0800

    add test route

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const indexRouter = require("./routes/indexRoutes");
 const playerRouter = require("./routes/playersRoutes");
 const characterRouter = require("./routes/charactersRoutes");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", indexRouter);
 app.use("/players", playerRouter);
 app.use("/characters", characterRouter);
 app.listen(process.env.APP_PORT, () => {
