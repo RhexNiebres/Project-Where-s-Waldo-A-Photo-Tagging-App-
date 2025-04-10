b45f54230236bcadf151c9168ae2f5768c1a7496
RhexNiebres <rhexniebres@gmail.com>
2025-04-10 12:03:47 +0800

    update cors

--- server/src/app.js
+++ server/src/app.js
@@ -6,11 +6,8 @@ const characterRouter = require("./routes/charactersRoutes");
 const indexRouter = require("./routes/indexRoutes");
 const cors = require("cors");
 
-app.use(cors({
-  origin: GAME_CLIENT_HOST, //Netlify URL
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors({ origin: "*",}));
+app.options("*", cors());
 
 app.use(express.json());
 app.use("/players", playerRouter);
