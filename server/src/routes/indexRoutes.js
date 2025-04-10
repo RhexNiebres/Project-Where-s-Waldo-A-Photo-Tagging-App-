4a1d28d692e271cb4b58aba402a46cbb419079d3
RhexNiebres <rhexniebres@gmail.com>
2025-04-10 10:45:20 +0800

    rename file

--- server/src/routes/indexRoutes.js
+++ server/src/routes/indexRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-  res.send("Hello, World from server!");
+  res.send("this is the test server!");
 });
 
 module.exports = router;
